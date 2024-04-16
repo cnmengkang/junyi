43fb219e9c9baeb55964843d646d299575bdc177
zkmeng <zkmeng@leqibike.com>
2024-04-16 18:24:16 +0800

    1111

--- src/views/download.vue
+++ src/views/download.vue
@@ -89,7 +89,7 @@ onMounted(() => {
   getAjax();
 });
 let getAjax = () => {
-  fetch("/src/data.json")
+  fetch("data.json")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
